867f986da9e20fe63c639a0736690513148088e2
Mansi <mpatel.y@gmail.com>
2016-01-21 15:44:58 -0500

    rename

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ http.createServer(function (req, res) {
 }).listen(port, hostname, function () {
     console.log("Server running at http://" + hostname + ":" + port);
 });
-//# sourceMappingURL=index.js.map
+//# sourceMappingURL=server.js.map
