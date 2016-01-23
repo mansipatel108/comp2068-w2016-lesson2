4af3df4943caadae776ad87f86e56d955eb4813e
Mansi <mpatel.y@gmail.com>
2016-01-23 08:57:46 -0500

    updated server.js

--- server.ts
+++ server.ts
@@ -1,14 +1,11 @@
 import http = require('http');
 
-var hostname:string = '127.0.0.1';
-var port:number = 8080;
+var port:number = process.env.PORT || 8080;
 
 http.createServer(function(req:http.ServerRequest, res:http.ServerResponse){
 res.writeHead(200, {'Conntent-Type': 'text/plain'});
  res.end('Hello World\n');
-}).listen(port ,hostname,function(){
-    console.log("Server running at http://" +hostname+ ":" +port);
-});
+}).listen(port);
 
 
 
